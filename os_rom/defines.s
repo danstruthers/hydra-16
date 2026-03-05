ba503878397983648190b0713882c65c831f390b
Dan Struthers <dan.struthers@gmail.com>
2026-03-05 10:50:11 -0800

    Fixing up alignment

--- os_rom/defines.s
+++ os_rom/defines.s
@@ -625,7 +625,7 @@ F_RETURN_SIZE   := 24 * F_CELL
 .endif
 .endmacro
 
-.macro META_CALL       call, C1, C2, C3, C4, C5, C6, C7, C8, C9
+.macro META_CALL    call, C1, C2, C3, C4, C5, C6, C7, C8, C9
     .ifblank C2
                 LDA_CORA        {C1}
                 jsr             call
@@ -634,10 +634,10 @@ F_RETURN_SIZE   := 24 * F_CELL
                 lda             C1
                 jsr             call
     .endif
-                META_CALL      {call}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
+                META_CALL       {call}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
 .endmacro
 
-.macro META_JMP    call, C1, C2, C3, C4, C5, C6, C7, C8, C9
+.macro META_JMP     call, C1, C2, C3, C4, C5, C6, C7, C8, C9
     .ifblank C2
                 LDA_CORA        {C1}
                 jmp             call
@@ -646,7 +646,7 @@ F_RETURN_SIZE   := 24 * F_CELL
                 lda             C1
                 jsr             call
     .endif
-                META_JMP {call}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
+                META_JMP        {call}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
 .endmacro
 
 .macro  PRINT_CHAR      C1, C2, C3, C4, C5, C6, C7, C8, C9
@@ -670,23 +670,23 @@ F_RETURN_SIZE   := 24 * F_CELL
 .endmacro
 
 .macro  PRINT_BYTE      C1, C2, C3, C4, C5, C6, C7, C8, C9
-                META_CALL      WRITE_BYTE, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
+                META_CALL       WRITE_BYTE, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
 .endmacro
 
 .macro  PRINT_BYTE_JMP  C1, C2, C3, C4, C5, C6, C7, C8, C9
-                META_JMP  WRITE_BYTE, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
+                META_JMP        WRITE_BYTE, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
 .endmacro
 
 .macro  PRINT_HEX  C1, C2, C3, C4, C5, C6, C7, C8, C9
-                META_CALL      WRITE_HEX, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
+                META_CALL       WRITE_HEX, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
 .endmacro
 
 .macro  PRINT_HEX_JMP  C1, C2, C3, C4, C5, C6, C7, C8, C9
-                META_JMP  WRITE_HEX, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
+                META_JMP        WRITE_HEX, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
 .endmacro
 
 .macro  PRINT_HEX_MASK  C1, C2, C3, C4, C5, C6, C7, C8, C9
-                META_CALL      WRITE_HEX_MASK, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
+                META_CALL       WRITE_HEX_MASK, {C1}, {C2}, {C3}, {C4}, {C5}, {C6}, {C7}, {C8}, {C9}
 .endmacro
 
 .macro  PRINT_CRLF
